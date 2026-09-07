93b695561f01a237f56c207cb3f0b84a94db7a64
Jaskarandeep Bhandol <jbhandol@rrc.ca>
2026-09-07 14:11:44 -0500

    Created employee directory using HTML Structure

---
+++ script.js
@@ -0,0 +1,44 @@
+// array of employees to be displayed on the page
+const employees = [
+    {
+        firstName: "Zoë",
+        lastName: "Robins",
+        department: "Administration"
+    },
+    {
+        firstName: "Madeleine",
+        lastName: "Madden",
+        department: "Administration"
+    }
+];
+
+const populateEmployeeList = (mainNode, employees) => {
+
+    const departmentSection = document.createElement("section");
+
+    const departmentHeading = document.createElement("h2");
+    departmentHeading.innerHTML = "Adminstration";
+
+    departmentSection.appendChild(departmentHeading);
+
+    const employeeList = document.createElement("ul");
+    departmentSection.appendChild(employeeList);
+
+     employees.forEach(employee => {
+        const employeeLi = document.createElement("li");
+
+        employeeLi.innerHTML = `${employee.firstName} ${employee.lastName}`;
+
+        employeeList.appendChild(employeeLi);
+    });
+      
+    mainNode.appendChild(departmentSection);
+};
+
+// add event listener when the DOM has loaded fully
+document.addEventListener("DOMContentLoaded", () => {
+
+    const mainNode = document.querySelector("main");
+
+    populateEmployeeList(mainNode, employees);
+});
